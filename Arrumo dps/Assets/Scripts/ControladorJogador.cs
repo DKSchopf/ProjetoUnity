aa867827143310bbb549d2b0a68da1b9e8f52beb
DKSchopf <DKSchopf@users.noreply.github.com>
2020-06-01 14:50:00 -0300

    NaoFuncionaa

---
+++ Arrumo dps/Assets/Scripts/ControladorJogador.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorJogador : MonoBehaviour
+{
+    private ControladorMovimentacao controladorMovimentacao;
+
+    public float velocidadeMovimento = 1000f;
+
+    public float alturaPulo = 5000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controladorMovimentacao = GetComponent<ControladorMovimentacao>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direcao = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direcao -= 1f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direcao += 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            controladorMovimentacao.Pular(alturaPulo * Time.deltaTime);
+        }
+
+        controladorMovimentacao.Mover(direcao * velocidadeMovimento * Time.deltaTime);
+    }
+}
+
+   
---
+++ Arrumo dps/Assets/Scripts/ControladorMovimentacao.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorMovimentacao : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    public void Mover(float velocidade)
+    {
+        rb.AddForce(new Vector2(velocidade, 0));
+    }
+
+    public void Pular(float velocidade)
+    {
+        rb.AddForce(new Vector2(0, velocidade));
+    }
+}
